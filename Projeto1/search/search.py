22e3605ca0cea89dcf1ef5f24a8103d8653dca50
Felipe Fontenele <felipefontenele1317@gmail.com>
2021-08-14 20:16:33 -0300

    Remove useless comments

--- Projeto1/search/search.py
+++ Projeto1/search/search.py
@@ -86,34 +86,6 @@ def depthFirstSearch(problem):
     print "Is the start a goal?", problem.isGoalState(problem.getStartState())
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
-    # fringe = util.Stack()
-    # initial_position = problem.getStartState()
-    # fringe.push(initial_position)
-    # # print "Fringe: ",fringe
-    # # print("Start:", problem.getStartState())
-    # # print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    # # print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    # # input()
-
-    # visited_location = set()
-
-    # while not fringe.isEmpty():
-    #     node = fringe.pop()
-    #     print "Sei la: ", node
-    #     visited_location.add(node[0])
-    #     if problem.isGoalState(node[0]):
-    #         return node[1]
-    #     successors = problem.getSuccessors(node)
-    #     for successor in successors:
-    #         if successor[0] in visited_location:
-    #             continue
-    #         fringe.push((successor[0], successor[1]))
-    #         print "Sucessor: ", successor
-
-    # "*** YOUR CODE HERE ***"
-    # from game import Directions
-    # s = Directions.SOUTH
-    # w = Directions.WEST
     
     stack = util.Stack()
     start_location = problem.getStartState()
